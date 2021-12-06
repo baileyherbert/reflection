f6789abb0a1eb011b37b6bbc962ffc054a694684
Bailey Herbert <hello@bailey.sh>
2021-12-06 05:59:53 -0700

    Add documentation to the meta decorator

--- src/decorators/Meta.ts
+++ src/decorators/Meta.ts
@@ -45,10 +45,52 @@ let AutoMeta = function(metadataKey: any, metadataValue: any): MetaReturnType {
 	}
 };
 
+interface MetaDecorator {
+	/**
+	 * A smart metadata decorator that can set metadata on classes, methods, and parameters.
+	 *
+	 * @param metadataKey The key to set
+	 * @param metadataValue The value to set
+	 */
+	(metadataKey: any, metadataValue: any): MetaReturnType;
+
+	/**
+	 * A metadata decorator that can only be applied to classes.
+	 *
+	 * @param metadataKey The key to set
+	 * @param metadataValue The value to set
+	 */
+	Class: typeof ClassMeta,
+
+	/**
+	 * A metadata decorator that can only be applied to methods.
+	 *
+	 * @param metadataKey The key to set
+	 * @param metadataValue The value to set
+	 */
+	Method: typeof MethodMeta,
+
+	/**
+	 * A metadata decorator that can only be applied to parameters.
+	 *
+	 * @param metadataKey The key to set
+	 * @param metadataValue The value to set
+	 */
+	Parameter: typeof ParameterMeta
+}
+
 /**
- * This is a utility decorator that allows you to easily set metadata on a class, method, or parameter.
+ * A smart metadata decorator that can set metadata on classes, methods, and parameters.
+ *
+ * - `Meta()` – Works anywhere
+ * - `Meta.Class()` – Classes only
+ * - `Meta.Method()` – Methods only
+ * - `Meta.Parameter()` – Parameters only
+ *
+ * @param metadataKey The key to set
+ * @param metadataValue The value to set
  */
-export const Meta = Object.assign(
+export const Meta: MetaDecorator = Object.assign(
 	AutoMeta,
 	{ Class: ClassMeta },
 	{ Method: MethodMeta },
